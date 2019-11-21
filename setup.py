3e49376a740ed0fccbc69f190412f4c151c3037b
gbayarri <genis.bayarri@irbbarcelona.org>
2019-11-21 19:57:16 +0100

    Documentation

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
         "Bioexcel": "https://bioexcel.eu/"
     },
     packages=setuptools.find_packages(exclude=['docs', 'test',]),
-    install_requires=['biobb_common==1.1.6', 'biobb_structure_checking==1.0.6'],
+    install_requires=['biobb_common==2.0.1', 'biobb_structure_checking==1.0.6'],
     python_requires='==3.6.*',
     classifiers=(
         "Development Status :: 3 - Alpha",
