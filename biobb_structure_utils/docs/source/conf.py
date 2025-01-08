4cd44b944906c89e96a43098b1328e87dc704a06
gbayarri <genis.bayarri@irbbarcelona.org>
2025-01-08 16:36:02 +0100

    Year updated

--- biobb_structure_utils/docs/source/conf.py
+++ biobb_structure_utils/docs/source/conf.py
@@ -65,7 +65,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "biobb_structure_utils"
-copyright = "2024, Bioexcel Project"
+copyright = "2025, Bioexcel Project"
 author = "Bioexcel Project"
 
 # The version info for the project you're documenting, acts as replacement for
